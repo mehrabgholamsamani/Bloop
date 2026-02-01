70a37aa0417982ecf5604cfaa1a720b130b0af6f
Mehrab Gholamsamani <mehrabgholamsamani@gmail.com>
2026-02-01 05:16:54 +0100

    feat: begin migration.sql

---
+++ apps/server/prisma/migrations/20260713010000_replies_reactions/migration.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "Message" ADD COLUMN "parentId" UUID;
+CREATE INDEX "Message_parentId_idx" ON "Message"("parentId");
+ALTER TABLE "Message" ADD CONSTRAINT "Message_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Message"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+CREATE TABLE "Reaction" (
+  "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+  "messageId" UUID NOT NULL,
+  "userId" UUID NOT NULL,
