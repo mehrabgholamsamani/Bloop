170ed5a4ddacb4d8520024a66fbab33361ae53f9
Mehrab Gholamsamani <mehrabgholamsamani@gmail.com>
2026-03-19 02:34:38 +0100

    feat: complete password-hash.ts

--- apps/server/src/password-hash.ts
+++ apps/server/src/password-hash.ts
@@ -1,2 +1,5 @@
 import argon2 from 'argon2';
 
+const password = process.argv[2];
+if (!password) throw new Error('Usage: pnpm --filter @chatroom/server password:hash <password>');
+console.log(await argon2.hash(password, { type: argon2.argon2id }));
